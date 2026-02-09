ce7c6b00aea6954231c5daabc9fad16359445a76
Ee Loong <eeloonglow@gmail.com>
2026-02-09 23:23:37 +0800

    WIP

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -6,3 +6,36 @@ export const me = {
 	LinkedIn: 'https://www.linkedin.com/in/low-ee-loong/',
 	Email: 'mailto:eeloonglow@gmail.com'
 };
+
+export const education = [
+	{
+		id: 1,
+		schools: [
+			{
+				name: 'DigiPen Institute of Technology',
+				logo: '/images/DigiPen_RGB_Red.png'
+			},
+			{
+				name: 'Singapore Institute of Technology',
+				logo: '/images/SITlogo.svg'
+			}
+		],
+		degree: 'Bachelor of Science in Computer Science',
+		period: '2023 - 2027',
+		status: 'Current',
+		isCurrent: true
+	},
+	{
+		id: 2,
+		schools: [
+			{
+				name: 'Ngee Ann Polytechnic',
+				logo: '/images/NPlogo.png'
+			}
+		],
+		degree: 'Diploma in Information Technology',
+		period: '2020 - 2023',
+		status: 'Completed',
+		isCurrent: false
+	}
+];
